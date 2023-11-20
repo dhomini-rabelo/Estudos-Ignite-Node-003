11d0f1c1398080542dd7329e51021bd0b361d177
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-11-20 02:08:15 -0300

    feat[addressGenerator]: add addressGenerator

--- practice03/src/domain/pet-app/application/use-cases/institution/create-institution.spec.ts
+++ practice03/src/domain/pet-app/application/use-cases/institution/create-institution.spec.ts
@@ -1,17 +1,25 @@
 import { InMemoryInstitutionRepository } from '@/domain/pet-app/__tests__/repositories/institution'
 import { CreateInstitutionUseCase } from './create-institution'
+import { MockAddressGenerator } from '@/domain/pet-app/__tests__/mocks/address'
 
 describe('Create a institution', () => {
   const institutionRepository = new InMemoryInstitutionRepository()
-  const sut = new CreateInstitutionUseCase(institutionRepository)
+  const mockAddressGenerator = new MockAddressGenerator()
+  const sut = new CreateInstitutionUseCase(
+    institutionRepository,
+    mockAddressGenerator,
+  )
 
   it('should create a institution', async () => {
     const response = await sut.execute({
       name: 'test',
+      zipCode: '12345-780',
     })
     expect(response.id.toString()).toEqual(expect.any(String))
     expect(
-      (await institutionRepository.findUniqueById(response.id.toString())).isEqual(response)
+      (
+        await institutionRepository.findUniqueById(response.id.toString())
+      ).isEqual(response),
     ).toBeTruthy()
   })
 })
--- practice03/src/domain/pet-app/application/use-cases/institution/create-institution.ts
+++ practice03/src/domain/pet-app/application/use-cases/institution/create-institution.ts
@@ -1,13 +1,28 @@
 import { IInstitutionRepository } from '@/domain/pet-app/ports/database/repositories/institution'
 import { IBaseUseCase } from '../../core/use-cases/base'
-import { IInstitutionProps, Institution } from '@/domain/pet-app/enterprise/entities/institution'
+import {
+  IInstitutionProps,
+  Institution,
+} from '@/domain/pet-app/enterprise/entities/institution'
+import { Address } from '@/domain/pet-app/enterprise/value-objects/address'
+import { IAddressGenerator } from '@/domain/pet-app/ports/address/contracts'
+
+interface IRequest extends Omit<IInstitutionProps, 'address'> {
+  zipCode: string
+}
 
 export class CreateInstitutionUseCase implements IBaseUseCase {
-  constructor(private institutionRepository: IInstitutionRepository) { }
+  constructor(
+    private institutionRepository: IInstitutionRepository,
+    private addressGenerator: IAddressGenerator,
+  ) { }
 
-  async execute(request: IInstitutionProps): Promise<Institution> {
+  async execute(request: IRequest): Promise<Institution> {
     return this.institutionRepository.create({
       ...request,
+      address: new Address(
+        await this.addressGenerator.fromZipCode(request.zipCode),
+      ),
     })
   }
 }
