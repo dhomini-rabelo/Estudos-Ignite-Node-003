b7b983ff9e324adccb8164de98b8e762e7c87226
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-11-20 12:10:18 -0300

    feat: add getters to Pet

--- practice03/src/domain/pet-app/enterprise/entities/pet.ts
+++ practice03/src/domain/pet-app/enterprise/entities/pet.ts
@@ -3,9 +3,23 @@ import { ID } from '../core/entities/id'
 
 export interface IPetProps {
   name: string
+  IBGECode: string
+  institutionId: ID
 }
 
 export class Pet extends IEntity<IPetProps> {
+  get institutionId() {
+    return this.props.institutionId
+  }
+
+  get IBGECode() {
+    return this.props.IBGECode
+  }
+
+  get name() {
+    return this.props.name
+  }
+
   static create(props: IPetProps) {
     return new Pet(props)
   }
