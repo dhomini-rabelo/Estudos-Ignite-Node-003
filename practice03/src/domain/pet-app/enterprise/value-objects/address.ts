b62d82c94d941592075f971ab0a46f254e371fb3
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-11-20 12:08:40 -0300

    feat: add getters to Address

--- practice03/src/domain/pet-app/enterprise/value-objects/address.ts
+++ practice03/src/domain/pet-app/enterprise/value-objects/address.ts
@@ -1,11 +1,31 @@
 export interface IAddress {
   zipCode: string
-  ibgeCode: string
+  IBGECode: string
   city: string
   state: string
   number: string
 }
 
 export class Address {
+  get IBGECode() {
+    return this.value.IBGECode
+  }
+
+  get zipCode() {
+    return this.value.zipCode
+  }
+
+  get city() {
+    return this.value.city
+  }
+
+  get state() {
+    return this.value.state
+  }
+
+  get number() {
+    return this.value.number
+  }
+
   constructor(private value: IAddress) { }
 }
