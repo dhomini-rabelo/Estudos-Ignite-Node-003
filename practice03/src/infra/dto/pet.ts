61dd512520b8cd02caa4603ab173c8dd6e6eebdf
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-12-02 20:40:09 -0300

    feat: PetDTO

---
+++ practice03/src/infra/dto/pet.ts
@@ -0,0 +1,45 @@
+import { ErrorMessages } from '@/domain/bounded-contexts/pet-app/application/core/errors/validation-error/messages'
+import * as zod from 'zod'
+
+export const PetDTO = zod.object({
+  name: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  description: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  animalType: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  lifeStage: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  size: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  energyLevel: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  independenceLevel: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  environmentSize: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  IBGECode: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+  institutionId: zod.string({
+    required_error: ErrorMessages.REQUIRED,
+    invalid_type_error: ErrorMessages.INVALID_VALUE,
+  }),
+})
