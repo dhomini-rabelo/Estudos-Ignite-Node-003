9e3dd455cd71c50d1092572383dfd96d8ef35fec
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-12-02 22:11:20 -0300

    minor style

--- practice03/src/domain/core/entities/types.ts
+++ practice03/src/domain/core/entities/types.ts
@@ -3,13 +3,3 @@ import { ID } from './id'
 export type WithID<Props> = Props & {
   id: ID
 }
-
-export type ConditionalType<T> = T extends null ? string : number
-
-export type MappedType<T> = {
-  [K in keyof T]: T[K] | null
-}
-
-export type TrueMappedType<T> = {
-  [K in keyof T]: true
-}
